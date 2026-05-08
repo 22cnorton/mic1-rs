563b8aaa500039adfb2d6f94709ff50c40691592
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-05-07 23:28:11 -0400

    fix: registers not working

--- src/registers.rs
+++ src/registers.rs
@@ -3,154 +3,140 @@ use core::fmt;
 pub type RegisterSize = u16;
 
 #[derive(Eq, PartialEq, Debug, Clone, Copy, Hash)]
-pub struct Registers([RegisterSize; 16]);
-// #[derive(Eq, PartialEq, Debug, Clone, Copy, Hash)]
-// pub struct Registers {
-
-//     pc: RegisterSize,
-//     ac: RegisterSize,
-//     sp: RegisterSize,
-//     ir: RegisterSize,
-//     tir: RegisterSize,
-//     zero: RegisterSize,
-//     one: RegisterSize,
-//     neg_one: RegisterSize,
-//     amask: RegisterSize,
-//     smask: RegisterSize,
-//     a: RegisterSize,
-//     b: RegisterSize,
-//     c: RegisterSize,
-//     d: RegisterSize,
-//     e: RegisterSize,
-//     f: RegisterSize,
-// }
+pub struct Registers {
+    pc: RegisterSize,
+    ac: RegisterSize,
+    sp: RegisterSize,
+    ir: RegisterSize,
+    tir: RegisterSize,
+    zero: RegisterSize,
+    one: RegisterSize,
+    neg_one: RegisterSize,
+    amask: RegisterSize,
+    smask: RegisterSize,
+    a: RegisterSize,
+    b: RegisterSize,
+    c: RegisterSize,
+    d: RegisterSize,
+    e: RegisterSize,
+    f: RegisterSize,
+}
 
 impl fmt::Display for Registers {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(
             f,
             "\n{:<15}: {1:016b}  base 10: {1:7}",
-            "ProgramCounter",
-            self.pc()
+            "ProgramCounter", self.pc
         )?;
         writeln!(
             f,
             "{:<15}: {1:016b}  base 10: {1:7}",
-            "Accumulator",
-            self.ac()
+            "Accumulator", self.ac
         )?;
         writeln!(
             f,
             "{:<15}: {1:016b}  base 10: {1:7}",
-            "InstructionReg",
-            self.ir()
+            "InstructionReg", self.ir
         )?;
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "TempInstr", self.tir)?;
         writeln!(
             f,
             "{:<15}: {1:016b}  base 10: {1:7}",
-            "TempInstr",
-            self.tir()
+            "StackPointer", self.sp
         )?;
-        writeln!(
-            f,
-            "{:<15}: {1:016b}  base 10: {1:7}",
-            "StackPointer",
-            self.sp()
-        )?;
-        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "ARegister", self.a())?;
-        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "BRegister", self.b())?;
-        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "CRegister", self.c())?;
-        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "DRegister", self.d())?;
-        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "ERegister", self.e())?;
-        write!(f, "{:<15}: {1:016b}  base 10: {1:7}", "FRegister", self.f())
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "ARegister", self.a)?;
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "BRegister", self.b)?;
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "CRegister", self.c)?;
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "DRegister", self.d)?;
+        writeln!(f, "{:<15}: {1:016b}  base 10: {1:7}", "ERegister", self.e)?;
+        write!(f, "{:<15}: {1:016b}  base 10: {1:7}", "FRegister", self.f)?;
+
+        Ok(())
     }
 }
 
 impl Registers {
-    const STACK_POINTER: usize = 2;
-    const PROGRAM_COUNTER: usize = 0;
-    const ACCUMULATOR: usize = 1;
-    const INSTRUCTION_REGISTER: usize = 3;
-    const TEMP_INSTRUCTION_REGISTER: usize = 4;
-    const A_REGISTER: usize = 10;
-    const B_REGISTER: usize = 11;
-    const C_REGISTER: usize = 12;
-    const D_REGISTER: usize = 13;
-    const E_REGISTER: usize = 14;
-    const F_REGISTER: usize = 15;
-
     pub fn new(stack_pointer: RegisterSize, program_counter: RegisterSize) -> Self {
-        let mut arr = [Default::default(); 16];
-        arr[Self::STACK_POINTER] = stack_pointer;
-        arr[Self::PROGRAM_COUNTER] = program_counter;
-        Self(arr)
-    }
-
-    fn a(&self) -> RegisterSize {
-        self.0[Self::A_REGISTER]
-    }
-    fn b(&self) -> RegisterSize {
-        self.0[Self::B_REGISTER]
-    }
-    fn c(&self) -> RegisterSize {
-        self.0[Self::C_REGISTER]
-    }
-    fn d(&self) -> RegisterSize {
-        self.0[Self::D_REGISTER]
-    }
-    fn e(&self) -> RegisterSize {
-        self.0[Self::E_REGISTER]
-    }
-    fn f(&self) -> RegisterSize {
-        self.0[Self::F_REGISTER]
-    }
-
-    fn ac(&self) -> RegisterSize {
-        self.0[Self::ACCUMULATOR]
-    }
-
-    fn ir(&self) -> RegisterSize {
-        self.0[Self::INSTRUCTION_REGISTER]
-    }
-    fn tir(&self) -> RegisterSize {
-        self.0[Self::TEMP_INSTRUCTION_REGISTER]
+        Self {
+            pc: program_counter,
+            sp: stack_pointer,
+            ..Default::default()
+        }
     }
 
     pub fn sp(&self) -> RegisterSize {
-        self.0[Self::STACK_POINTER]
+        self.sp
     }
     pub fn pc(&self) -> RegisterSize {
-        self.0[Self::PROGRAM_COUNTER]
+        self.pc
     }
 
     pub fn read_from_reg(&self, index: u8) -> RegisterSize {
-        self.0[index as usize]
+        match index {
+            0 => self.pc,
+            1 => self.ac,
+            2 => self.sp,
+            3 => self.ir,
+            4 => self.tir,
+            5 => self.zero,
+            6 => self.one,
+            7 => self.neg_one,
+            8 => self.amask,
+            9 => self.smask,
+            10 => self.a,
+            11 => self.b,
+            12 => self.c,
+            13 => self.d,
+            14 => self.e,
+            15 => self.f,
+            _ => panic!("Invalid register index: {}", index),
+        }
     }
 
     pub fn write_to_reg(&mut self, index: u8, value: RegisterSize) {
-        self.0[index as usize] = value;
+        // eprintln!("Writing value {:016b} to register index {}", value, index);
+        match index {
+            0 => self.pc = value,
+            1 => self.ac = value,
+            2 => self.sp = value,
+            3 => self.ir = value,
+            4 => self.tir = value,
+            5 => self.zero = value,
+            6 => self.one = value,
+            7 => self.neg_one = value,
+            8 => self.amask = value,
+            9 => self.smask = value,
+            10 => self.a = value,
+            11 => self.b = value,
+            12 => self.c = value,
+            13 => self.d = value,
+            14 => self.e = value,
+            15 => self.f = value,
+            _ => panic!("Invalid register index: {}", index),
+        }
     }
 }
 
 impl Default for Registers {
     fn default() -> Self {
-        Self([
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            (0),
-            (1),
-            (u16::MAX),
-            (0x0FFF),
-            (0x00FF),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-        ])
+        Self {
+            pc: Default::default(),
+            ac: Default::default(),
+            sp: Default::default(),
+            ir: Default::default(),
+            tir: Default::default(),
+            zero: (0),
+            one: (1),
+            neg_one: (u16::MAX),
+            amask: (0x0FFF),
+            smask: (0x00FF),
+            a: Default::default(),
+            b: Default::default(),
+            c: Default::default(),
+            d: Default::default(),
+            e: Default::default(),
+            f: Default::default(),
+        }
     }
 }
