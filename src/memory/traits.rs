f61661faaed6a4691cdf132971e8090120ee87e6
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-06-06 18:03:12 -0400

    split Mutable Memory Trait

--- src/memory/traits.rs
+++ src/memory/traits.rs
@@ -8,7 +8,16 @@ pub trait ReadableMemory {
 }
 
 pub trait WritableMemory{
-    
+    type MemoryType;
+    type MemoryError;
+    fn write(&mut self, index: usize, value: Self::MemoryType) -> Result<(), Self::MemoryError>;
+}
+
+pub trait MutableReadableMemory {
+
+    type MemoryType;
+    type MemoryError;
+    fn read(&mut self, index: usize) -> Result<&Self::MemoryType, Self::MemoryError>;
 }
 
 pub trait MutableMemory: ReadableMemory {
