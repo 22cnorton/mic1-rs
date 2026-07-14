806fc3820dd1babda5acb6b59c30d14fa6996cb8
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-07-14 17:16:44 -0400

    refactor: add help text to cli

--- src/cli.rs
+++ src/cli.rs
@@ -12,16 +12,19 @@ use crate::machine::{Machine, Mic1Error};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub(crate) struct Mic1Args {
-    #[arg(long)]
+    #[arg(
+        long,
+        help = "Path to PROM file. If not provided, the default PROM will be used"
+    )]
     prom: Option<PathBuf>,
 
-    #[arg(long)]
+    #[arg(long, help = "Path to program file to run, as binary strings")]
     program: PathBuf,
 
-    #[arg(long, default_value_t = 0)]
+    #[arg(long, default_value_t = 0, help = "Initial program counter value")]
     program_counter: u16,
 
-    #[arg(long, default_value_t = 0x0F80)]
+    #[arg(long, default_value_t = 0x0F80, help = "Initial stack pointer value")]
     stack_pointer: u16,
 }
 
@@ -30,7 +33,7 @@ impl Mic1Args {
         self.prom.as_ref()
     }
 
-    pub(crate) fn prom_data(&self) -> impl Iterator<Item = String>{
+    pub(crate) fn prom_data(&self) -> impl Iterator<Item = String> {
         if let Some(path) = &self.prom {
             if let Ok(file) = File::open(path) {
                 return Either::Left(io::BufReader::new(file).lines().flatten());
