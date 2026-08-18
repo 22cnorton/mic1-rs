cca26d46242a8aaabe2c63476811ae06f36a7813
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-08-18 18:57:11 -0400

    feat: implement LineAccessor

--- emulator/src/memory/io_memory/access.rs
+++ emulator/src/memory/io_memory/access.rs
@@ -5,3 +5,7 @@ pub trait LineGetter<T> {
 pub trait LineSender<T> {
     fn send_line(&self, line: &T);
 }
+
+pub trait LineAccessor<T>: LineSender<T> + LineGetter<T> {}
+
+impl<T, R> LineAccessor<T> for R where Self: LineSender<T> + LineGetter<T> {}
