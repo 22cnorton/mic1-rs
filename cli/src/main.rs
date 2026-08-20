870b65056d037c417f545211c36771f3d79ccc8a
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-08-20 14:54:05 -0400

    refactor: remove second thread for running emulator

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,12 +9,10 @@ use emulator::{
     },
 };
 use flume::Receiver;
-use messages::{Command, Event};
 use std::io::{self, Write, stdin, stdout};
 
 mod cli;
 mod io_access;
-mod messages;
 
 macro_rules! print_mem {
     ($addr:expr, $value:expr) => {{
@@ -34,88 +32,38 @@ macro_rules! print_mem {
 }
 
 fn main() -> anyhow::Result<()> {
-    let (command_tx, command_rx) = flume::bounded(1);
-    let (event_tx, event_rx) = flume::unbounded();
-    let (input_tx, input_rx) = flume::unbounded(); // TODO: channel to send input to the emulator thread
-    let (output_tx, output_rx) = flume::unbounded(); // TODO: channel to send output from the emulator thread
-    let emulator_input_rx = input_rx.clone();
+    let (input_tx, input_rx) = flume::unbounded();
+    let (output_tx, output_rx) = flume::unbounded();
 
-    std::thread::spawn(move || {
-        let args = Mic1Args::parse();
-        let io_accessor = ChannelLineAccessor {
-            tx: output_tx.clone(),
-            rx: emulator_input_rx,
-        };
-        let prom_data: Vec<_> = args.prom_data().collect();
-        let memory_data: Vec<_> = args.memory_data().unwrap().collect();
-        let read_micro_instructions = prom_data.len();
-        let read_machine_instructions = memory_data.len();
-        let mut emulator =
-            MachineBuilder::default() //TODO: rewrite to use if let and emit failed to init on failure
-                .micro_code(ImmutableMemory::from_binary_str_lines(prom_data).unwrap())
-                .memory(
-                    IOMemoryBuilder::<ChannelLineAccessor>::default()
-                        .memory(MutableMemory::from_binary_str_lines(memory_data).unwrap())
-                        .line_accessor(io_accessor)
-                        .build()
-                        .unwrap(),
-                )
-                .registers(
-                    RegistersBuilder::default()
-                        .sp(args.stack_pointer())
-                        .pc(args.program_counter())
-                        .build()
-                        .unwrap(),
-                )
-                .build()
-                .unwrap();
-        event_tx
-            .send(messages::Event::DoneInit {
-                sp: args.stack_pointer(),
-                pc: args.program_counter(),
-                read_micro_instructions,
-                read_machine_instructions,
-            })
-            .unwrap();
-        let mut halted = false;
-        loop {
-            if !halted {
-                let state = emulator.pulse(); // pulse machine
-                if let MachineState::Halted = state {
-                    event_tx.send(Event::Halted).unwrap();
-                    halted = true;
-                }
-            }
-            // manage commands
-            while let Ok(command) = command_rx.try_recv() {
-                // eprintln!("Processing {command:?}");/
-                event_tx
-                    .send(match command {
-                        Command::ViewMemory(indicies) => {
-                            Event::Memory(emulator.get_memory(indicies))
-                        }
-                        Command::ViewRegisters => Event::Registers(*emulator.registers()),
-                        Command::ViewMicrocode => Event::Microcode(emulator.microcode()),
-                        Command::Quit => return,
-                        Command::Continue => {
-                            halted = false;
-                            emulator.r#continue();
-                            break;
-                        }
-                        Command::ViewCycles => Event::Cycles(*emulator.clock().tick()),
-                    })
-                    .unwrap();
-            }
-        }
-    });
+    let args = Mic1Args::parse();
+    let io_accessor = ChannelLineAccessor {
+        tx: output_tx,
+        rx: input_rx.clone(),
+    };
+    let prom_data: Vec<_> = args.prom_data().collect();
+    let memory_data: Vec<_> = args.memory_data()?.collect();
+    let read_micro_instructions = prom_data.len();
+    let read_machine_instructions = memory_data.len();
+    let mut emulator = MachineBuilder::default() //TODO: rewrite to use if let and emit failed to init on failure
+        .micro_code(ImmutableMemory::from_binary_str_lines(prom_data)?)
+        .memory(
+            IOMemoryBuilder::default()
+                .memory(MutableMemory::from_binary_str_lines(memory_data)?)
+                .line_accessor(io_accessor)
+                .build()?,
+        )
+        .registers(
+            RegistersBuilder::default()
+                .sp(args.stack_pointer())
+                .pc(args.program_counter())
+                .build()?,
+        )
+        .build()?;
 
     std::thread::spawn(move || {
         for line in stdin().lines() {
             if let Ok(line) = line {
-                let mut bytes = line;
-                bytes.push('\n');
-
-                if matches!(input_tx.send(bytes), Err(_)) {
+                if matches!(input_tx.send(line + "\n"), Err(_)) {
                     return;
                 }
             }
@@ -125,60 +73,43 @@ fn main() -> anyhow::Result<()> {
     let mut state = EmulatorState::Init;
     let input_buffer = input_rx.clone();
 
-    while !event_rx.is_disconnected() {
-        // while let Ok(event) = event_rx.recv() {
+    loop {
+        for line in output_rx.try_iter() {
+            print!("{line}");
+        }
 
         state = match state {
             EmulatorState::Init => {
-                let event = event_rx.recv()?;
-                if let Event::DoneInit {
-                    read_micro_instructions,
-                    read_machine_instructions,
-                    sp,
-                    pc,
-                } = event
-                {
-                    println!("Read in {} micro instructions", read_micro_instructions);
-                    println!("Read in {} machine instructions", read_machine_instructions);
-                    println!("{:<15}: {1:016b}  base 10: {1:7}", "Starting PC is", pc);
-                    println!("{:<15}: {1:016b}  base 10: {1:7}", "Starting SP is", sp);
-                    println!();
-                }
+                println!("Read in {} micro instructions", read_micro_instructions);
+                println!("Read in {} machine instructions", read_machine_instructions);
+                println!(
+                    "{:<15}: {1:016b}  base 10: {1:7}",
+                    "Starting PC is",
+                    emulator.registers().pc()
+                );
+                println!(
+                    "{:<15}: {1:016b}  base 10: {1:7}",
+                    "Starting SP is",
+                    emulator.registers().sp()
+                );
+                println!();
+
                 EmulatorState::Processing
             }
 
-            EmulatorState::Processing => flume::Selector::new()
-                .recv(&event_rx, |event| match event {
-                    Ok(Event::Halted) => EmulatorState::Stats,
-                    _ => EmulatorState::Processing,
-                })
-                .recv(&output_rx, |output| {
-                    if let Ok(line) = output {
-                        print!("{line}");
-
-                        stdout().flush().unwrap();
-                    }
-                    EmulatorState::Processing
-                })
-                .wait(),
+            EmulatorState::Processing => match emulator.pulse() {
+                MachineState::Halted => EmulatorState::Stats,
+                MachineState::Running => EmulatorState::Processing,
+            },
             EmulatorState::ShowPC => {
-                command_tx.send(Command::ViewRegisters)?;
-                if let Event::Registers(registers) = event_rx.recv()? {
-                    println!("\nThe new PC is  : {:016b}\n", registers.pc());
-                }
-                command_tx.send(Command::Continue)?;
+                println!("\nThe new PC is  : {:016b}\n", emulator.registers().pc());
+
                 EmulatorState::Processing
             }
             EmulatorState::Stats => {
-                command_tx.send(Command::ViewRegisters)?;
-                if let Event::Registers(registers) = event_rx.recv()? {
-                    println!("{registers}\n");
-                }
+                println!("{}\n", emulator.registers());
 
-                command_tx.send(Command::ViewCycles)?;
-                if let Event::Cycles(cycles) = event_rx.recv()? {
-                    println!("{:<15}: {}\n", "Total Cycles", cycles);
-                }
+                println!("{:<15}: {}\n", "Total Cycles", emulator.clock().tick());
 
                 EmulatorState::Menu
             }
@@ -186,56 +117,39 @@ fn main() -> anyhow::Result<()> {
                 MenuOptions::Quit => EmulatorState::Quit,
                 MenuOptions::Continue => EmulatorState::ShowPC,
                 MenuOptions::ViewMicrocode => EmulatorState::DisplayMicrocode,
-                MenuOptions::ViewMemory(index) => {
-                    command_tx.send(Command::ViewMemory(vec![index]))?;
-                    EmulatorState::MemoryMenu
-                }
+                MenuOptions::ViewMemory(index) => EmulatorState::MemoryMenu(index),
             },
-            EmulatorState::MemoryMenu => {
-                let index = if let Event::Memory(memory_map) = event_rx.recv()? {
-                    let index = memory_map[0].0;
-                    print_mem!(memory_map);
-                    index
-                } else {
-                    Default::default()
-                };
+            EmulatorState::MemoryMenu(index) => {
+                print_mem!(emulator.get_memory([index]));
 
                 match memory_submenu(index, &input_buffer) {
                     Some(
                         MemorySubmenuOptions::Forward { indices }
                         | MemorySubmenuOptions::Backward { indices },
-                    ) => {
-                        command_tx.send(Command::ViewMemory(indices))?;
-                        EmulatorState::DisplayMemory
-                    }
+                    ) => EmulatorState::DisplayMemory(indices),
                     Some(MemorySubmenuOptions::Quit) => EmulatorState::Quit,
                     Some(MemorySubmenuOptions::Continue) | None => EmulatorState::Menu,
                 }
             }
 
-            EmulatorState::DisplayMemory => {
-                if let Event::Memory(memory_map) = event_rx.recv()? {
-                    print_mem!(memory_map);
-                }
+            EmulatorState::DisplayMemory(memory_indicies) => {
+                let memory_map = emulator.get_memory(memory_indicies);
+                print_mem!(memory_map);
 
                 EmulatorState::Menu
             }
             EmulatorState::DisplayMicrocode => {
-                command_tx.send(Command::ViewMicrocode)?;
-                if let Event::Microcode(micro_code) = event_rx.recv()? {
-                    for (i, instruction) in micro_code.iter().enumerate() {
-                        println!("{:>4}: {instruction:?}", i.saturating_add(1));
-                    }
+                for (i, instruction) in emulator.microcode().iter().enumerate() {
+                    println!("{:>4}: {instruction:?}", i.saturating_add(1));
                 }
 
                 EmulatorState::Menu
             }
             EmulatorState::Quit => {
-                command_tx.send(Command::Quit)?;
                 println!("MIC-1 emulator finishing, goodbye");
                 break;
             }
-        }
+        };
     }
 
     Ok(())
@@ -318,13 +232,13 @@ fn get_line(input_buffer: &Receiver<String>) -> Option<String> {
     input_buffer.recv().ok().map(|str| str.trim().to_string())
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum EmulatorState {
     Init,
     Processing,
     Menu,
-    MemoryMenu,
-    DisplayMemory,
+    MemoryMenu(usize),
+    DisplayMemory(Vec<usize>),
     DisplayMicrocode,
     ShowPC,
     Stats,
